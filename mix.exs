1981d955047f290c2b157bcb38bff492c72f8cb3
diceandink <136260551+diceandink@users.noreply.github.com>
2025-12-28 04:25:53 +0300

    Basic Phoenix setup and dependencies

---
+++ mix.exs
@@ -0,0 +1,39 @@
+defmodule Ratelimit.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :ratelimit,
+      version: "0.1.0",
+      elixir: "~> 1.14",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      mod: {Ratelimit.Application, []},
+      extra_applications: [:logger, :runtime_tools]
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  defp deps do
+    [
+      {:phoenix, "~> 1.7.0"},
+      {:jason, "~> 1.2"},
+      {:plug_cowboy, "~> 2.5"}
+    ]
+  end
+
+  defp aliases do
+    [
+      setup: ["deps.get"]
+    ]
+  end
+end
